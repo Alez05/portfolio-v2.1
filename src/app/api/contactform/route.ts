d183c94d75e085a2f5f50e710ad682f36c022262
alez05 <cercel.alexandru436@gmail.com>
2025-03-24 15:24:23 +0000

    Add Error Handling - Ensure proper error handling for form submission and data fetching. Fixes #18

--- src/app/api/contactform/route.ts
+++ src/app/api/contactform/route.ts
@@ -1,6 +1,27 @@
 import { NextResponse } from 'next/server'
-import { getFormData } from './contactform'
 
-export const GET = async () => {
-  return NextResponse.json(getFormData)
+export const POST = async (request: Request) => {
+  try {
+    const data = await request.json()
+
+    if (!data.name || !data.email || !data.message) {
+      return NextResponse.json(
+        { error: 'All fields are required' },
+        { status: 400 }
+      )
+    }
+
+    console.log('Form recived', data)
+
+    return NextResponse.json(
+      { message: 'Form submitted succesfully' },
+      { status: 200 }
+    )
+  } catch (error) {
+    console.log('there is an error', error)
+    return NextResponse.json(
+      { error: 'Internal server error.' },
+      { status: 500 }
+    )
+  }
 }
--- src/libs/ui/organism/contact/contactform/action.ts
+++ src/libs/ui/organism/contact/contactform/action.ts
@@ -1,37 +1,42 @@
 'use server'
+import { getFormData } from '@api/contactform/contactform'
 
 import { TFormField } from './contactform.type'
 
 export const getContactFormAction = async (): Promise<TFormField> => {
-  try {
-    const response = await fetch('http://localhost:3000/api/contactform')
-
-    if (!response.ok) {
-      throw new Error(
-        `Faile to get the contact form data: ${response.statusText}`
-      )
-    }
-    return (await response.json()) as TFormField
-  } catch (error) {
-    console.error('Error fetching contact form data:', error)
-    return {
-      form: {
-        header: {
-          subtitle: '',
-          title: '',
-          description: '',
-          link: {
-            text: '',
-            cta: '',
-          },
-        },
-        field: [],
-      },
-      button: {
-        id: '',
-        label: '',
-        type: 'submit',
-      },
-    }
-  }
+  return getFormData
 }
+
+//   {
+//   try {
+//     const response = await fetch('http://localhost:3000/api/contactform')
+
+//     if (!response.ok) {
+//       throw new Error(
+//         `Faile to get the contact form data: ${response.statusText}`
+//       )
+//     }
+//     return (await response.json()) as TFormField
+//   } catch (error) {
+//     console.error('Error fetching contact form data:', error)
+//     return {
+//       form: {
+//         header: {
+//           subtitle: '',
+//           title: '',
+//           description: '',
+//           link: {
+//             text: '',
+//             cta: '',
+//           },
+//         },
+//         field: [],
+//       },
+//       button: {
+//         id: '',
+//         label: '',
+//         type: 'submit',
+//       },
+//     }
+//   }
+// }
